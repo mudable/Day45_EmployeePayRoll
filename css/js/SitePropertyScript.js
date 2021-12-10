24b20e448f5902c468106ff2c9baaf64a862b1fe
mudable <mudableshilpa@gmail.com>
2021-12-10 13:10:03 +0530

    first commit

---
+++ css/js/SitePropertyScript.js
@@ -0,0 +1,4 @@
+let site_properties = {
+    home_page: "C:\\Users\\HP WORLD\\Desktop\\Day45_EmployeePayRoll\\css\\js\\html\\Home.html",
+    add_emp_payroll_page: "C:\\Users\\HP WORLD\\Desktop\\Day45_EmployeePayRoll\\css\\js\\html"
+};
